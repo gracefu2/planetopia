108716285676dbf52479624fa854db91a73b840a
Grace Fu <12668088+gracefu2@users.noreply.github.com>
2024-10-20 14:36:19 -0500

    Update Login background color to bluewhite.js

--- app/screens/signedOutStack/Login.js
+++ app/screens/signedOutStack/Login.js
@@ -58,7 +58,7 @@ export default LoginScreen
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'red',
+    backgroundColor: '#e5efff',
     justifyContent: 'center',
     padding: sizing.defaultContainerPadding,
     gap: sizing.defaultContainerPadding
