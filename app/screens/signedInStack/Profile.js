92c36280f22160ef8feb1f3a9bb610ef2b5cc11c
Grace Fu <12668088+gracefu2@users.noreply.github.com>
2024-10-22 23:12:50 -0500

    Update Profile.js fix badges

--- app/screens/signedInStack/Profile.js
+++ app/screens/signedInStack/Profile.js
@@ -6,7 +6,6 @@ import { LineChart } from 'react-native-svg-charts';
 import { useUser } from '../../../context/UserContext';
 import { Ionicons } from '@expo/vector-icons';
 import Avatar from '../../components/Avatar';
-import { LinearGradient } from 'expo-linear-gradient'; // Make sure to install this package
 
 const ProfileScreen = () => {
   const navigation = useNavigation();
@@ -20,71 +19,71 @@ const ProfileScreen = () => {
   const userData = useUser();
 
   return (
-    <LinearGradient colors={['#b3d99e', '#71cabb', '#2cbbd9']} style={styles.container}>
-      <ScrollView>
-        <TouchableOpacity onPress={() => navigation.navigate('Settings')}>
-          <Ionicons name="settings-outline" size={24} color="#FF6F61" />
-        </TouchableOpacity>
-        {/* Section 1: Streak, Following, Followers, Avatar */}
-        <View style={styles.section}>
-          <View style={styles.profileInfo}>
-            <Avatar type={userData.profilePic} />
-            <View style={styles.infoText}>
-              <Text style={styles.streak}>{userData.name}</Text>
-              <Text style={styles.uname}>{userData.username}</Text>
-              <Text style={styles.follow}>
-                Joined {new Date(userData.dateJoined).toLocaleDateString('en-US', { month: 'long', year: 'numeric' })}
-              </Text>
-            </View>
-          </View>
-          <View style={styles.statsContainer}>
-            <View style={styles.statItem}>
-              <Text style={styles.statLabel}>Streak</Text>
-              <Text style={styles.statValue}>15</Text>
-            </View>
-            <View style={styles.statItem}>
-              <Text style={styles.statLabel}>Friends</Text>
-              <Text style={styles.statValue}>105</Text>
-            </View>
-            <View style={styles.statItem}>
-              <Text style={styles.statLabel}>Points</Text>
-              <Text style={styles.statValue}>155</Text>
-            </View>
-          </View>
-        </View>
-
-        {/* Section 2: Progress */}
-        <View style={styles.section}>
-          <Text style={styles.sectionTitle}>Progress</Text>
-          <Button text="Navigate to Planet Zones" onPress={() => navigation.navigate('habitat')} />
-        </View>
-
-        {/* Section 3: Achievements and Badges */}
-        <View style={styles.section}>
-          <Text style={styles.sectionTitle}>Achievements and Badges</Text>
-          {achievements.map((achievement, index) => (
-            <Text key={index} style={styles.achievement}>
-              {achievement}
+    <ScrollView style={styles.container}>
+      <TouchableOpacity onPress={() => navigation.navigate('Settings')}>
+        <Ionicons name="settings-outline" size={24} color="#4CAF50" />
+      </TouchableOpacity>
+      {/* Section 1: Streak, Following, Followers, Avatar */}
+      <View style={styles.section}>
+        <View style={styles.profileInfo}>
+          <Avatar type={userData.profilePic} />
+          <View style={styles.infoText}>
+            <Text style={styles.streak}>{userData.name}</Text>
+            <Text style={styles.uname}>{userData.username}</Text>
+            <Text style={styles.follow}>
+              Joined {new Date(userData.dateJoined).toLocaleDateString('en-US', { month: 'long', year: 'numeric' })}
             </Text>
+          </View>
+        </View>
+        <View style={styles.statsContainer}>
+          <View style={styles.statItem}>
+            <Text style={styles.statLabel}>Streak</Text>
+            <Text style={styles.statValue}>15</Text>
+          </View>
+          <View style={styles.statItem}>
+            <Text style={styles.statLabel}>Friends</Text>
+            <Text style={styles.statValue}>105</Text>
+          </View>
+          <View style={styles.statItem}>
+            <Text style={styles.statLabel}>Points</Text>
+            <Text style={styles.statValue}>155</Text>
+          </View>
+        </View>
+      </View>
+
+      {/* Section 2: Progress */}
+      <View style={styles.section}>
+        <Text style={styles.sectionTitle}>Progress</Text>
+        <Button text="Navigate to Planet Zones" onPress={() => navigation.navigate('habitat')} />
+      </View>
+
+      {/* Section 3: Achievements and Badges */}
+      <View style={styles.section}>
+        <Text style={styles.sectionTitle}>Achievements and Badges</Text>
+        <ScrollView horizontal showsHorizontalScrollIndicator={false} style={styles.badgesContainer}>
+          {achievements.map((achievement, index) => (
+            <View key={index} style={styles.badge}>
+              <Text style={styles.badgeText}>{achievement}</Text>
+            </View>
           ))}
-        </View>
+        </ScrollView>
+      </View>
 
-        {/* Section 5: Total and Monthly Reports */}
-        <View style={styles.section}>
-          <Text style={styles.sectionTitle}>Total and Monthly Reports</Text>
-          <Text style={styles.reportText}>Summarize your activities, achievements, and overall impact.</Text>
-          {/* <LineChart
-            style={{ height: 200 }}
-            data={data}
-            contentInset={{ top: 20, bottom: 20 }}
-            svg={{ fill: 'rgba(134, 65, 244, 0.8)' }}
-          /> */}
-          <Text style={styles.graphLabel}>Your Progress Over Time</Text>
-        </View>
+      {/* Section 5: Total and Monthly Reports */}
+      <View style={styles.section}>
+        <Text style={styles.sectionTitle}>Total and Monthly Reports</Text>
+        <Text style={styles.reportText}>Summarize your activities, achievements, and overall impact.</Text>
+        {/* <LineChart
+          style={{ height: 200 }}
+          data={data}
+          contentInset={{ top: 20, bottom: 20 }}
+          svg={{ fill: 'rgba(134, 65, 244, 0.8)' }}
+        /> */}
+        <Text style={styles.graphLabel}>Your Progress Over Time</Text>
+      </View>
 
-        <View style={{ height: 50 }} />
-      </ScrollView>
-    </LinearGradient>
+      <View style={{ height: 50 }} />
+    </ScrollView>
   );
 };
 
@@ -93,6 +92,7 @@ export default ProfileScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#E1F5FE', // Light blue background for vibrance
     paddingHorizontal: 16,
     paddingVertical: 50,
   },
@@ -125,12 +125,12 @@ const styles = StyleSheet.create({
   uname: {
     fontSize: 16,
     marginTop: 4,
-    color: '#333', // Text color for username
+    color: '#b3d99e', // Secondary color for username
   },
   follow: {
     fontSize: 14,
     marginTop: 4,
-    color: '#333', // Text color for joined date
+    color: '#71cabb', // Accent color for joined date
   },
   sectionTitle: {
     fontSize: 26,
@@ -148,26 +148,45 @@ const styles = StyleSheet.create({
   },
   statLabel: {
     fontSize: 16,
-    color: '#b3d99e', // Secondary color for stat labels
+    color: '#8E24AA', // Purple for stat labels
   },
   statValue: {
     fontSize: 26,
     fontWeight: 'bold',
-    color: '#71cabb', // Accent color for stat values
+    color: '#FF9800', // Bright orange for stat values
+  },
+  badgesContainer: {
+    flexDirection: 'row',
+    marginTop: 10,
+    paddingVertical: 10,
+  },
+  badge: {
+    backgroundColor: '#b3d99e', // Secondary color for badges
+    borderRadius: 50, // Circle shape
+    paddingVertical: 12,
+    paddingHorizontal: 20,
+    marginRight: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  badgeText: {
+    color: '#fff', // White text for badges
+    fontSize: 16,
+    fontWeight: 'bold',
   },
   achievement: {
     fontSize: 18,
     marginBottom: 4,
-    color: '#333', // Text color for achievements
+    color: '#4CAF50', // Green for achievements
   },
   reportText: {
     fontSize: 18,
-    color: '#333', // Text color for report text
+    color: '#333', // Dark grey for report text
   },
   graphLabel: {
     textAlign: 'center',
     marginTop: 16,
     fontSize: 18,
-    color: '#333', // Text color for graph label
+    color: '#795548', // Brown for graph label
   },
 });
