13518db68a64cc4613bcdb15cf5b7e74e3541052
Grace Fu <12668088+gracefu2@users.noreply.github.com>
2024-10-23 00:17:41 -0500

    Update Feed.js

--- app/screens/signedInStack/Feed.js
+++ app/screens/signedInStack/Feed.js
@@ -1,20 +1,21 @@
-import { ActivityIndicator, Image, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import React, { useEffect, useState } from 'react'
-import sizing from '../../../theme/sizing'
-import colors from '../../../theme/colors'
-import getNewsData from '../../../utils/getNewsData'
-import getGreeting from '../../../utils/getGreeting'
-import { useUser } from '../../../context/UserContext'
+import { ActivityIndicator, Image, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import sizing from '../../../theme/sizing';
+import colors from '../../../theme/colors';
+import getNewsData from '../../../utils/getNewsData';
+import getGreeting from '../../../utils/getGreeting';
+import { useUser } from '../../../context/UserContext';
+import { LinearGradient } from 'expo-linear-gradient';
 
 const FeedScreen = () => {
-  const userData = useUser()
-  const message = getGreeting(userData?.name)
+  const userData = useUser();
+  const message = getGreeting(userData?.name);
 
   const categories = [
     "All", "Climate Change", "Renewable", "Environmental Compliance", "Engineering", "Sustainability"
-  ]
+  ];
 
-  const [selectedCategory, setSelectedCategory] = useState(categories[0])
+  const [selectedCategory, setSelectedCategory] = useState(categories[0]);
   const [categorizedArticles, setCategorizedArticles] = useState({
     "All": [], 
     "Climate Change": [],
@@ -22,108 +23,109 @@ const FeedScreen = () => {
     "Environmental Compliance": [],
     "Engineering": [],
     "Sustainability": []
-  })
+  });
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchNews = async () => {
       getNewsData().then(response => {
-        let newCategorizedAricles = {
+        let newCategorizedArticles = {
           "All": response.data.articles.results, 
           "Climate Change": [],
           "Renewable": [],
           "Environmental Compliance": [],
           "Engineering": [],
           "Sustainability": []
-        }
+        };
         response.data.articles.results.forEach(data => {
           const cats = data.categories.map(cat => {
-            const splits = cat.label.split("/")
-            return splits[splits.length-1]
-          })
-      
-          const catsFiltered = cats.filter(function(item, pos) {
-            return cats.indexOf(item) == pos;
-          })
+            const splits = cat.label.split("/");
+            return splits[splits.length - 1];
+          });
+
+          const catsFiltered = cats.filter((item, pos) => {
+            return cats.indexOf(item) === pos;
+          });
 
           catsFiltered.forEach(cat => {
-            if (newCategorizedAricles[cat]) {
-              newCategorizedAricles[cat].push(data)
+            if (newCategorizedArticles[cat]) {
+              newCategorizedArticles[cat].push(data);
             }
-          })
-        })
+          });
+        });
 
-        setCategorizedArticles(newCategorizedAricles)
-        setLoading(false)
+        setCategorizedArticles(newCategorizedArticles);
+        setLoading(false);
       }).catch(err => {
         console.error(err);
         setLoading(false);
-      })
-    }
+      });
+    };
 
     fetchNews();
   }, []);
 
-  if (loading) return <View style={styles.container}>
-    <View style={styles.section}>
-      <Text style={styles.title}>{message}</Text>
-      <Text>{new Date().toLocaleDateString('en-US', {weekday: 'long', month: 'long', day: '2-digit', year: 'numeric'})}</Text>
-    </View>
-    <ActivityIndicator />
-  </View>
+  if (loading) {
+    return (
+      <LinearGradient colors={['#b3d99e', '#71cabb', '#2cbbd9']} style={styles.container}>
+        <View style={styles.section}>
+          <Text style={styles.title}>{message}</Text>
+          <Text>{new Date().toLocaleDateString('en-US', { weekday: 'long', month: 'long', day: '2-digit', year: 'numeric' })}</Text>
+        </View>
+        <ActivityIndicator />
+      </LinearGradient>
+    );
+  }
 
-  console.log(categorizedArticles[selectedCategory][0]["source"]["title"])
   return (
-    <ScrollView style={styles.container}>
-      <View style={styles.section}>
-        <Text style={styles.title}>{message}</Text>
-        <Text>{new Date().toLocaleDateString('en-US', {weekday: 'long', month: 'long', day: '2-digit', year: 'numeric'})}</Text>
-      </View>
-      <ScrollView horizontal={true} contentContainerStyle={styles.categoriesContainer} showsHorizontalScrollIndicator={false}>
-        <View style={{width: sizing.defaultContainerPadding-sizing.sm}} />
-        {
-          categories.map(text => 
+    <LinearGradient colors={['#b3d99e', '#71cabb', '#2cbbd9']} style={styles.container}>
+      <ScrollView>
+        <View style={styles.section}>
+          <Text style={styles.title}>{message}</Text>
+          <Text>{new Date().toLocaleDateString('en-US', { weekday: 'long', month: 'long', day: '2-digit', year: 'numeric' })}</Text>
+        </View>
+        <ScrollView horizontal={true} contentContainerStyle={styles.categoriesContainer} showsHorizontalScrollIndicator={false}>
+          <View style={{ width: sizing.defaultContainerPadding - sizing.sm }} />
+          {categories.map(text => 
             <TouchableOpacity
               key={text}
               onPress={() => setSelectedCategory(text)}
-              style={[styles.categoryButton, selectedCategory===text && {backgroundColor: colors.extraLightBlue}]}
+              style={[styles.categoryButton, selectedCategory === text && { backgroundColor: colors.extraLightBlue }]}
             >
-              <Text style={[styles.categoryText, selectedCategory===text && {color: colors.blue}]}>{text}</Text>
+              <Text style={[styles.categoryText, selectedCategory === text && { color: colors.blue }]}>{text}</Text>
             </TouchableOpacity>
-          )
-        }
-        <View style={{width: sizing.defaultContainerPadding-sizing.sm}} />
-      </ScrollView>
+          )}
+          <View style={{ width: sizing.defaultContainerPadding - sizing.sm }} />
+        </ScrollView>
 
-      <View style={[styles.section, {gap: sizing.lg}]} >
-        {
-          categorizedArticles[selectedCategory]?.map(article =>
-          <View style={{flexDirection: 'column', backgroundColor: colors.white, borderRadius: sizing.sm}} key={article.url}>
-            
-            {
-              article.image && 
-              <Image style={{objectFit: 'cover', borderTopLeftRadius: sizing.sm, borderTopRightRadius: sizing.sm, height: 150, width: "100%"}} source={{uri: article.image}} />
-            }
-            <View style={{width: '100%', flexShrink: 1, padding: sizing.md}}>
-              <Text style={styles.articleTitle}>{article.title}</Text>
-              <Text style={[styles.articleDescription, {color: colors.midGray}]}>{article["source"]["title"] && article["source"]["title"]+" · "}{new Date(article.dateTime).toLocaleString('en-US', {month: '2-digit', day: '2-digit', year: 'numeric', hour12: true, hour: '2-digit', minute: '2-digit'})}</Text>
-              
-              <Text style={styles.articleDescription}>{article.body.slice(0, 100)}...</Text>
+        <View style={[styles.section, { gap: sizing.lg }]}>
+          {categorizedArticles[selectedCategory]?.map(article => 
+            <View style={{ flexDirection: 'column', backgroundColor: colors.white, borderRadius: sizing.sm }} key={article.url}>
+              {article.image && 
+                <Image style={{ objectFit: 'cover', borderTopLeftRadius: sizing.sm, borderTopRightRadius: sizing.sm, height: 150, width: "100%" }} source={{ uri: article.image }} />
+              }
+              <View style={{ width: '100%', flexShrink: 1, padding: sizing.md }}>
+                <Text style={styles.articleTitle}>{article.title}</Text>
+                <Text style={[styles.articleDescription, { color: colors.midGray }]}>
+                  {article.source.title ? `${article.source.title} · ` : ''}
+                  {new Date(article.dateTime).toLocaleString('en-US', { month: '2-digit', day: '2-digit', year: 'numeric', hour12: true, hour: '2-digit', minute: '2-digit' })}
+                </Text>
+                <Text style={styles.articleDescription}>{article.body.slice(0, 100)}...</Text>
+              </View>
             </View>
-          </View>)
-        }
-      </View>
-    </ScrollView>
-  )
-}
+          )}
+        </View>
+      </ScrollView>
+    </LinearGradient>
+  );
+};
 
-export default FeedScreen
+export default FeedScreen;
 
 const styles = StyleSheet.create({
   container: {
     paddingVertical: sizing.xxxl,
-    backgroundColor: colors.offWhite,
-    flex: 1
+    flex: 1,
   },
   title: {
     fontSize: sizing.lg,
@@ -157,4 +159,4 @@ const styles = StyleSheet.create({
     fontSize: 14,
     marginTop: 8,
   },
-})
+});
